2dfb2be8b79f5deb566237277f908426ae2d96db
julietasegovia <julietavsegovia@gmail.com>
2026-05-07 23:26:48 -0300

    styiling searches

--- src/MealSearch.jsx
+++ src/MealSearch.jsx
@@ -15,20 +15,20 @@ export default function MealSearch() {
     const { data, loading, error } = useMeals(`/search.php?s=${debouncedQuery}`);
 
     return(
-        <div>
-            <div className="items-center flex justify-center pt-16">
-                <input value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Look for a recipe..." className="border rounded-md w-3/4 h-10 text-center flex justify-center text-gray-700"/>
+        <div className="flex justify-center flex-col items-center">
+            <div className="items-center flex justify-center pt-16 w-full">
+                <input value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Look for a recipe..." className="border rounded-md h-10 text-center flex justify-center text-gray-700 w-1/2"/>
             </div>
 
             {loading && <p className="text-center pt-10">Loading...</p>}
             {error && <p className="text-center pt-10">Error: {error}</p>}
 
-            <div className="pt-10 justify-center pl-56 items-center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-16 w-full">
+            <div className="pt-10 justify-center items-center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-16">
                 {data?.meals?.map((meal) => (
-                    <div key={meal.idMeal}>
-                        <h3>{meal.strMeal}</h3>
-                        <img src={meal.strMealThumb} alt={meal.strMeal} width={300} className="rounded-3xl"/>
-                        <p>{meal.strCategory} . {meal.strArea}</p>
+                    <div key={meal.idMeal} className="flex justify-center flex-col text-center items-center w-full">
+                        <h3 className="text-lg font-semibold">{meal.strMeal}</h3>
+                            <img src={meal.strMealThumb} alt={meal.strMeal} width={280} className="cursor-pointer rounded-3xl"/>
+                            <p>{meal.strCategory}</p> <p className="text-gray-700">{meal.strArea}</p>
                     </div>
                 ))}
             </div>
